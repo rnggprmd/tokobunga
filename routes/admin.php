3aa351167827e5d2abe3961c40c31fcf3b011bf0
rnggprmd <ranggaprmdy1@gmail.com>
2026-04-12 23:27:24 +0700

    perubahan

--- routes/admin.php
+++ routes/admin.php
@@ -49,19 +49,24 @@ Route::middleware(['auth', 'admin'])->group(function () {
         Route::get('/products', [ProductController::class, 'index'])->name('products.index');
         Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
         Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+        // Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
         Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
         Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
         Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
 
         // Product Variants
         Route::get('/variants', [ProductVariantController::class, 'index'])->name('variants.index');
+        // Route::get('/variants/create', [ProductVariantController::class, 'create'])->name('variants.create');
         Route::post('/variants', [ProductVariantController::class, 'store'])->name('variants.store');
+        // Route::get('/variants/{variant}/edit', [ProductVariantController::class, 'edit'])->name('variants.edit');
         Route::put('/variants/{variant}', [ProductVariantController::class, 'update'])->name('variants.update');
         Route::delete('/variants/{variant}', [ProductVariantController::class, 'destroy'])->name('variants.destroy');
 
         // Categories
         Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+        // Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
         Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+        // Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
         Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
         Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
     });
